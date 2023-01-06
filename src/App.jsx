60e06e910a776bb4baebc274e74a69d185ae955b
jony <sabinalatipova20@gmail.com>
2023-01-07 04:59:25 +0500

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.scss";
+import HomePage from "./Pages/HomePage";
+
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="">
+			<HomePage />
+		</div>
+	)
 }
 
 export default App;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from "../Components/Header";
+
+const HomePage = () => {
+   return (
+      <div className="">
+         <Header />
+      </div>
+   );
+}
+
+export default HomePage;
