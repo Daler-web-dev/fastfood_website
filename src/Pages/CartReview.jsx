673eb3b48791c35559150b179c0455c33b31f928
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-01-11 19:53:25 +0500

    add:CardReview page

--- src/Pages/CartReview.jsx
+++ src/Pages/CartReview.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CartReviewChild from "../Components/children/CartReviewChild";
+import { TbPencil } from "react-icons/tb";
+import { IoMdContact } from "react-icons/io";
+import { BiTimeFive } from "react-icons/bi";
+import { GiMoneyStack } from "react-icons/gi";
 
 function CartReview() {
   const [blockClick, setBlockClick] = useState(false);
@@ -8,16 +12,18 @@ function CartReview() {
   return (
     <div className="mx-16">
       <div className="flex items-center gap-3 mb-6">
-        <h1 className="text-[30px] lg:text-[40px]">Обзор</h1>
+        <h1 className="lg:text-4xl font-semibold text-2xl ">
+          Оформление заказа
+        </h1>
       </div>
       <div className="flex w-full gap-5 flex-col lg:flex-row">
-        <div className="w-full">
+        <div className="w-full flex flex-col gap-5">
           <div
             onClick={() => setBlockClick(!blockClick)}
             className={
               blockClick
-                ? "w-full shadow-md rounded-lg overflow-hidden h-auto"
-                : "w-full shadow-md rounded-lg overflow-hidden h-[55px]"
+                ? "w-full shadow-md rounded-lg overflow-hidden h-auto bg-[#F2F2F2]"
+                : "w-full shadow-md rounded-lg overflow-hidden h-[55px] bg-[#F2F2F2]"
             }
           >
             <div className="flex items-center justify-between p-4 cursor-pointer">
@@ -46,8 +52,48 @@ function CartReview() {
               </p>
             </Link>
           </div>
-          <div>
-		  </div>
+          <div className="w-full flex items-center justify-between shadow-md rounded-lg p-4 bg-[#FFCCD5]">
+            <div className="flex items-center gap-3">
+              <img src="/icons/location.svg" className="w-7 h-7" alt="" />
+              <p className="text-[red]">Выберите адрес</p>
+            </div>
+            <TbPencil size="22" color="red" />
+          </div>
+          <div className="w-full flex items-center justify-between shadow-md rounded-lg p-4 bg-[#EBF3F2]">
+            <div className="flex items-center gap-3">
+              <IoMdContact size="40" color="grey" />
+              <div className="flex flex-col items-start">
+                <p className="font-black">User</p>
+                <p className="font-black">+998 (90) 123-45-67</p>
+              </div>
+            </div>
+            <TbPencil size="22" color="green" />
+          </div>
+          <div className="w-full flex items-center justify-between shadow-md rounded-lg p-4 bg-[#EBF3F2]">
+            <div className="flex items-center gap-3">
+              <BiTimeFive size="40" color="green" />
+              <div className="flex flex-col items-start">
+                <p className="font-bold text-[grey]">Время доставки</p>
+                <p className="font-black">Как можно быстрее</p>
+              </div>
+            </div>
+            <TbPencil size="22" color="green" />
+          </div>
+          <div className="w-full flex items-center justify-between shadow-md rounded-lg p-4 bg-[#EBF3F2]">
+            <div className="flex items-center gap-3">
+              <GiMoneyStack size="40" color="green" />
+              <div className="flex flex-col items-start">
+                <p className="font-bold text-[grey]">Способ оплаты</p>
+                <p className="font-black">Наличными (Без сдачи)</p>
+              </div>
+            </div>
+            <TbPencil size="22" color="green" />
+          </div>
+          <textarea
+            rows="4"
+            class="block p-2.5 w-full text-lg text-gray-900 rounded-lg mt-5 border shadow-sm outline-none"
+            placeholder="Комментарий"
+          ></textarea>
         </div>
         <div className="w-full flex flex-col gap-5 lg:w-1/2">
           <div class="w-full p-4 bg-white border rounded-lg flex items-center flex-col gap-3 shadow-md sm:p-8">
